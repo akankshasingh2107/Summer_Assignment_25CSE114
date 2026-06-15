4081687f8b4db93fc0b4f10a71ebbcdd33a91fe7
Akanksha Singh <21singhakanksha21@gmail.com>
2026-06-15 23:06:34 +0530

    Updated solutions for day12, day 13 and day14

---
+++ day14/q55.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int largest = arr[0], second = -999999;
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        } else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second Largest = %d", second);
+
+    return 0;
+}
